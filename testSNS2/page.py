78c73807d7d59e5f9be6622bb81931019d11331c
anzai.ryo0127 <36684885+anzairyo0127@users.noreply.github.com>
2018-05-06 10:40:09 +0900

    Add files via upload

---
+++ testSNS2/page.py
@@ -0,0 +1,35 @@
+import flask
+import sqlite3
+import sys
+
+# Flaskアプリ呼び出し、よくわからん
+app = flask.Flask(__name__)
+app.config['SECRET_KEY'] = 'pass'
+
+@app.route('/')
+def main():
+    return '''
+
+    '''
+@app.route('/login')
+def login():
+    flask.session['name'] = flask.request.args.get('name')
+    
+    return 'ようこそ' + flask.session['name'] + 'さん<br><a href="/mypage">マイページへ</a>'
+
+@app.route('/mypage')
+def mypage():
+    if 'name' in flask.session:
+        return flask.session['name'] + 'さんのページ<br><br><a href="/logout">ログアウト</a> <a href="/">トップページ</a>'
+    else:
+        return 'ログインしてください<br><br><a href="/">トップページ</a>'
+
+@app.route('/logout')
+def logout():
+    del flask.session['name']
+
+    return 'ログアウトしました<br><a href="/">トップページへ戻る</a>'
+
+
+if __name__ == "__main__":
+    app.run()
